63ceeb7a93b13f668ccbf62df5ff0be29de4daef
Evgeny Tukin <evgeny.tukin@gmail.com>
2023-03-17 20:47:42 +0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { Telegraf } from "telegraf";
-import axios from "axios";
+import { Configuration, OpenAIApi } from "openai";
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "";
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY || "";
@@ -15,23 +15,25 @@ if (!TELEGRAM_BOT_TOKEN || !OPENAI_API_KEY || !YOUR_USERNAME) {
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
-const openaiRequest = async (input: string): Promise<string> => {
-  const apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${OPENAI_API_KEY}`,
-  };
-  const data = {
-    prompt: input,
-    max_tokens: 50,
-    n: 1,
-    stop: null,
-    temperature: 0.7,
-  };
+let botUsername = "";
 
+bot.telegram.getMe().then((botInfo) => {
+  botUsername = botInfo.username;
+});
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const openaiRequest = async (input: string): Promise<string> => {
   try {
-    const response = await axios.post(apiUrl, data, { headers });
-    const generatedText = response.data.choices[0].text;
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    });
+    const generatedText = completion.data.choices[0].message?.content || "";
+
     return generatedText.trim();
   } catch (error) {
     console.error("Error with OpenAI API:", error);
@@ -49,11 +51,23 @@ bot.on("text", async (ctx) => {
   const input = ctx.message.text;
   const username = ctx.message.from?.username;
   const isReply = ctx.message.reply_to_message;
-  const isReplyToYourMessage =
-    isReply && ctx.message.reply_to_message?.from?.username === YOUR_USERNAME;
+  const isReplyToBotMessage =
+    isReply && ctx.message.reply_to_message?.from?.username === botUsername;
+  const isMentioned = input.includes(`@${botUsername}`);
 
-  if (username === YOUR_USERNAME || isReplyToYourMessage) {
-    const response = await openaiRequest(input);
+  console.log(
+    "all",
+    input,
+    username,
+    isReply,
+    isReplyToBotMessage,
+    isMentioned
+  );
+
+  if (isMentioned || isReplyToBotMessage) {
+    const response = await openaiRequest(
+      "отвечай как ватник и с матерком и юморком по-русски:\n\n" + input
+    );
     ctx.reply(response);
   }
 });
